2f53a63078135c2d41d9f95766100f7f996f796a
Phil R <philricardi@gmail.com>
2022-03-05 21:49:32 -0500

    added second page

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(
+      MaterialApp(
+        title: 'Named Routes Demo',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => FirstScreen(),
+          '/second': (context) => const SecondScreen(),
+        },
+      ),
+    );
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class FirstScreen extends StatelessWidget {
+  FirstScreen({Key? key}) : super(key: key);
+  final pageButtons = Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 26),
+          padding: const EdgeInsets.all(18),
+          primary: Colors.green[300],
+        ),
+        onPressed: () {},
+        child: const Text('Second Screen'),
+      ),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 26),
+          padding: const EdgeInsets.all(18),
+          primary: Colors.blue[500],
+        ),
+        onPressed: () {
+          //todo Navigate to third screen
+        },
+        child: const Text('Third Screen'),
+      ),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 26),
+          padding: const EdgeInsets.all(18),
+          primary: Colors.red[700],
+        ),
+        onPressed: () {
+          //todo Navigate to fourth screen
+        },
+        child: const Text('Fourth Screen'),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('my second app ish'),
+        title: const Text('Home Screen'),
         centerTitle: true,
         backgroundColor: Colors.red[500],
       ),
-      body: Row(
-        children: <Widget>[
-          const Text('Sup Dude'),
-          const Spacer(
-            flex: 2,
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            textStyle: const TextStyle(fontSize: 26),
+            padding: const EdgeInsets.all(18),
+            primary: Colors.green[300],
           ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              textStyle: const TextStyle(fontSize: 26),
-              padding: const EdgeInsets.all(18),
-              primary: Colors.amber,
-            ),
-            onPressed: () {},
-            child: const Text('click me'),
-          ),
-          const Spacer(
-            flex: 2,
-          ),
-          TextButton(
-            style: TextButton.styleFrom(
-              textStyle: const TextStyle(fontSize: 26),
-              padding: const EdgeInsets.all(12),
-              primary: Colors.purple,
-            ),
-            onPressed: () {},
-            child: const Text('Or This One'),
-          ),
-        ],
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SecondScreen()));
+          },
+          child: const Text('Second Screen'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
@@ -52,3 +81,24 @@ class Home extends StatelessWidget {
     );
   } // Widget build
 } //-- end class Home
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go Back'),
+        ),
+      ),
+    );
+  }
+} //-- end class SecondSreen
